8c75f952dc526b126b69bb226c5c856e41d3e1bb
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-08 02:15:39 +0200

    Add Core version contract

---
+++ src/lib_xdecarocore/src/Version.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package     Xdecaro.Core
+ * @subpackage  Library
+ *
+ * @copyright   Copyright (C) 2026 Luca De Caro
+ * @license     GNU General Public License version 2 or later
+ */
+
+namespace Xdecaro\Core;
+
+defined('_JEXEC') or die;
+
+final class Version
+{
+    public const VERSION = '1.0.0';
+
+    private function __construct()
+    {
+    }
+}
