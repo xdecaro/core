e4039e06fd7843c714fa6ba351dcd828a0c98ac1
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-08 02:16:27 +0200

    Add Core system plugin service provider

---
+++ src/plg_system_xdecarocore/services/provider.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package     Xdecaro.Core
+ * @subpackage  Plugin.System
+ *
+ * @copyright   Copyright (C) 2026 Luca De Caro
+ * @license     GNU General Public License version 2 or later
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+use Xdecaro\Plugin\System\XdecaroCore\Extension\XdecaroCore;
+
+return new class implements ServiceProviderInterface {
+    public function register(Container $container): void
+    {
+        $container->set(
+            PluginInterface::class,
+            static function (Container $container): XdecaroCore {
+                return new XdecaroCore(
+                    $container->get(DispatcherInterface::class),
+                    (array) PluginHelper::getPlugin('system', 'xdecarocore')
+                );
+            }
+        );
+    }
+};
