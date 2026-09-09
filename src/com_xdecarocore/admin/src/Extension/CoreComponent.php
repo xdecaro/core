1fd4ce39756f9b8d8e91f584f5c8c6ec22ec381d
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-09 19:44:34 +0200

    Add Core dashboard MVC component extension

---
+++ src/com_xdecarocore/admin/src/Extension/CoreComponent.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package     xdecaro.Core
+ * @subpackage  com_xdecarocore
+ */
+
+namespace xdecaro\Component\Core\Administrator\Extension;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\MVCComponent;
+
+final class CoreComponent extends MVCComponent
+{
+}
