7de87aa72a751dae1607e22c16fa582f8bf4f08a
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-10 22:40:19 +0200

    Add Core 2.x system plugin class

---
+++ src/plg_system_xdecarocore/src/Extension/CorePlugin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package     xdecaro.Core
+ * @subpackage  Plugin.System
+ *
+ * @copyright   Copyright (C) 2026 Luca De Caro
+ * @license     GNU General Public License version 2 or later
+ */
+
+namespace xdecaro\Plugin\System\Core\Extension;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Plugin\CMSPlugin;
+
+final class CorePlugin extends CMSPlugin
+{
+}
